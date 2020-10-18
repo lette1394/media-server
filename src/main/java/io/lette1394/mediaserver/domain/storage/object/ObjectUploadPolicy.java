e4e2f98e2459d737ac490f5877b7d4451c75d0f3
Jae Eun Na <jae-eun.na@linecorp.com>
2020-10-18 19:15:56 +0900

    Add policy

--- src/main/java/io/lette1394/mediaserver/domain/storage/object/ObjectUploadPolicy.java
+++ src/main/java/io/lette1394/mediaserver/domain/storage/object/ObjectUploadPolicy.java
@@ -1,9 +1,23 @@
 package io.lette1394.mediaserver.domain.storage.object;
 
+import com.google.common.base.Predicates;
+import java.util.Set;
 import java.util.function.Predicate;
+import lombok.Value;
 
+@FunctionalInterface
 public interface ObjectUploadPolicy {
   boolean test(Object object, Storage storage);
 
-  ObjectUploadPolicy ALLOW_RESUME_UPLOAD = (object, storage) -> true;
+  ObjectUploadPolicy ALLOW_RESUME_UPLOAD = (object, storage) -> false;
+
+  @Value
+  class AllMatch implements ObjectUploadPolicy {
+    Set<ObjectUploadPolicy> policies;
+
+    @Override
+    public boolean test(Object object, Storage storage) {
+      return policies.stream().allMatch(policy -> policy.test(object, storage));
+    }
+  }
 }
