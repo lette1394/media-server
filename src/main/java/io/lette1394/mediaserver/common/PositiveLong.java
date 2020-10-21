847f20e6355c2790753a5fb9ead253c14f150f2c
lette1394 <lette1394@gmail.com>
2020-10-22 03:20:31 +0900

    PositiveLong

--- src/main/java/io/lette1394/mediaserver/common/PositiveLong.java
+++ src/main/java/io/lette1394/mediaserver/common/PositiveLong.java
@@ -1,11 +1,15 @@
 package io.lette1394.mediaserver.common;
 
-import lombok.RequiredArgsConstructor;
 import lombok.Value;
 
 @Value
 public class PositiveLong {
   long value;
+
+  public PositiveLong(long value) {
+    Contracts.require(value > 0, "long value requires more than 0");
+    this.value = value;
+  }
 
   public static PositiveLong positiveLong(long value) {
     return new PositiveLong(value);
