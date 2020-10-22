c62b645133c7bd01cbecd4327a7ff93337d602bb
lette1394 <lette1394@gmail.com>
2020-10-23 00:13:16 +0900

    Add SingleThreadInputStreamPublisherTest

---
+++ src/test/java/io/lette1394/mediaserver/domain/storage/infrastructure/SingleThreadInputStreamPublisherTest.java
@@ -0,0 +1,21 @@
+package io.lette1394.mediaserver.domain.storage.infrastructure;
+
+import java.io.ByteArrayInputStream;
+import java.nio.ByteBuffer;
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
+class SingleThreadInputStreamPublisherTest {
+
+  @Test
+  void test1() {
+    final byte[] bytes = new byte[]{1, 2, 3, 4, 5};
+
+    StepVerifier
+      .create(new SingleThreadInputStreamPublisher(new ByteArrayInputStream(bytes), 2))
+      .expectNext(ByteBuffer.wrap(new byte[] {1,2}))
+      .expectNext(ByteBuffer.wrap(new byte[] {3,4}))
+      .expectNext(ByteBuffer.wrap(new byte[] {5}))
+      .verifyComplete();
+  }
+}
