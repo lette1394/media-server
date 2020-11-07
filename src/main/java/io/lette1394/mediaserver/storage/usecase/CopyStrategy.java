890e86625943bce59c7fc7a28fcc20a36541399c
Jae Eun Na <jae-eun.na@linecorp.com>
2020-11-08 03:11:51 +0900

    [wip-soft-copy] Add todos

--- src/main/java/io/lette1394/mediaserver/storage/usecase/CopyStrategy.java
+++ src/main/java/io/lette1394/mediaserver/storage/usecase/CopyStrategy.java
@@ -7,6 +7,10 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
 
+
+// TODO: 이거 음... 인터페이스가 사용하기에 좀 적절하지 않다.
+//  단순히 execute만 있어야 할 거 같은데... 조건문은 어디에 넣지?
+//  이거 matches() 가 아니라, pattern matching 으로 해야할 거 같다
 public interface CopyStrategy<BUFFER extends Payload> {
   boolean matches(long softCopiedCount);
 
@@ -27,8 +31,11 @@ public interface CopyStrategy<BUFFER extends Payload> {
       Object<BUFFER> sourceObject,
       Identifier targetIdentifier) {
 
-      //
-//      copyStrategies.stream()
+//      for (CopyStrategy<BUFFER> copyStrategy : copyStrategies) {
+//        if (copyStrategy.matches())
+//      }
+//
+//      copyStrategies.stream().filter()
 //        .filter(strategy -> strategy.matches(softCopiedCount));
       return null;
     }
