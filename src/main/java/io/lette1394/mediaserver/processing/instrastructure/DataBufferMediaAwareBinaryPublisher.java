446b7f5184314f2060a9f52cd9d11435a97cd327
lette1394 <lette1394@gmail.com>
2020-11-12 01:28:39 +0900

    [processing] Fix memory leak

--- src/main/java/io/lette1394/mediaserver/processing/instrastructure/DataBufferMediaAwareBinaryPublisher.java
+++ src/main/java/io/lette1394/mediaserver/processing/instrastructure/DataBufferMediaAwareBinaryPublisher.java
@@ -3,9 +3,9 @@ package io.lette1394.mediaserver.processing.instrastructure;
 import io.lette1394.mediaserver.processing.domain.MediaAwareBinaryPublisher;
 import io.lette1394.mediaserver.storage.domain.BinaryPublisher;
 import io.lette1394.mediaserver.storage.infrastructure.DataBufferPayload;
+import java.io.InputStream;
 import lombok.SneakyThrows;
 import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DataBufferUtils;
 
 public class DataBufferMediaAwareBinaryPublisher extends MediaAwareBinaryPublisher<DataBufferPayload> {
   public DataBufferMediaAwareBinaryPublisher(BinaryPublisher<DataBufferPayload> delegate, Listener listener) {
@@ -15,25 +15,12 @@ public class DataBufferMediaAwareBinaryPublisher extends MediaAwareBinaryPublish
   @Override
   @SneakyThrows
   protected byte[] getByte(DataBufferPayload payload) {
-    final DataBuffer dataBuffer = payload.getValue().retainedSlice(0, (int) payload.getSize());
-
-    DataBufferUtils.retain(payload.getValue());
-    DataBufferUtils.retain(payload.getValue());
-    DataBufferUtils.retain(payload.getValue());
-    DataBufferUtils.retain(payload.getValue());
-    DataBufferUtils.retain(payload.getValue());
-    DataBufferUtils.retain(payload.getValue());
-    DataBufferUtils.retain(dataBuffer);
-    DataBufferUtils.retain(dataBuffer);
-    DataBufferUtils.retain(dataBuffer);
-    DataBufferUtils.retain(dataBuffer);
-    DataBufferUtils.retain(dataBuffer);
-    DataBufferUtils.retain(dataBuffer);
-    DataBufferUtils.retain(dataBuffer);
-
-
+    final DataBuffer retainedSlice = payload.getValue().retainedSlice(0, (int) payload.getSize());
     byte[] ret = new byte[(int) payload.getSize()];
-    dataBuffer.asInputStream(false).read(ret);
+
+    try(final InputStream inputStream = retainedSlice.asInputStream(true)) {
+      inputStream.read(ret);
+    }
     return ret;
   }
 }
